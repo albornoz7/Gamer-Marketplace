a65d7b01e55ec4d19366290efcf6a0abebf50e41
albornoz7 <albornoz0313@gmail.com>
2023-11-10 15:45:29 -0500

    carrito 10-11-23

--- app/Http/Controllers/CarritoController.php
+++ app/Http/Controllers/CarritoController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Gloudemans\Shoppingcart\Cart;
-use App\Http\Controllers\ProductosController;
+use Cart;
+use App\Models\productos;
 
 class CarritoController extends Controller
 {
-    public function agregaritem(Resquest $resquest){
-        $producto = ProductosController::find($resquest->producto_id);
-        card::add({
-            'id'=> $producto->id,
-            'name'
-        })
+    public function agregaritem(Request $request){
+
+        $producto = Productos::find($request->producto_id);
+
+        Cart::add([
+            'id' => $producto->id,
+            'name' => $producto->nombre,
+            'price' => $producto->precio,
+            'qty' =>1,
+            
+        ]);
+    return redirect()->back();
+
+        
     }
 }
--- resources/views/Productos/categorias.blade.php
+++ resources/views/Productos/categorias.blade.php
@@ -2,12 +2,13 @@
 
 @section('content')
     <div class="grid grid-cols-1 md:grid-cols-4 ">
-        
-        @foreach($productos as $producto)
-            <div class="!z-5 relative m-4 rounded-[20px] max-w-[300px] bg-clip-border bg-gray-200 shadow-3xl shadow-lg flex flex-col w-full !p-4 3xl:p-![18px]  undefined">
+
+        @foreach ($productos as $producto)
+            <div
+                class="!z-5 relative m-4 rounded-[20px] max-w-[300px] bg-clip-border bg-gray-200 shadow-3xl shadow-lg flex flex-col w-full !p-4 3xl:p-![18px]  undefined">
                 <div class="h-full w-full">
                     <div class="relative w-full">
-                    <img src="{{ $producto->foto }}" alt="">
+                        <img src="{{ $producto->foto }}" alt="">
                     </div>
                     <div class="mb-3 flex items-end justify-end px-1 mt-4">
                         <div class="mb-2 ">
@@ -15,41 +16,46 @@
                             <p class="mt-1 text-sm font-medium text-gray-600 md:mt-2">{{ $producto->descripcion }}</p>
                             <p class="mt-1 text-sm font-medium text-gray-600 md:mt-2">Precio: {{ $producto->precio }}</p>
                         </div>
-                        <form action="{{route('agregaritem')}}" method="POST">
+                        <form action="{{ route('agregaritem') }}" method="POST">
                             @csrf
-                        <div class="flex md:mt-2 lg:mt-0">
-                            <input type="hidden" name="producto_id" value=" {{ $producto->precio }}" >
-                        <button href="" type="submit" class="linear rounded-[20px] bg-brand-900 px-4 py-2 text-xs font-medium  transition duration-200 hover:bg-brand-800 active:bg-brand-700 text-white">Agregar al carrito</button>
-                        </div>
-                    </form>
+                            <div class="flex md:mt-2 lg:mt-0">
+                                <input type="hidden" name="producto_id" value="{{ $producto->id }}">
+
+                                <button href="" type="submit"
+                                    class="linear rounded-[20px] bg-brand-900 px-4 py-2 text-xs font-medium  transition duration-200 hover:bg-brand-800 active:bg-brand-700 text-white">Agregar
+                                    al carrito</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                </div>
+            </div>
         @endforeach
         @if (count(Cart::content()))
-        <div class="grid grid-rows-3 grid-flow-col gap-4">
-            <p>Compras</p>
-            <table class="table-fixed">
-                <thead>
+            <div class="grid grid-rows-3 grid-flow-col gap-4">
+                <p>Compras</p>
+                <table class="table-fixed">
+                    <thead>
                         <tr>
-                        <th>Song</th>
-                        <th>Artist</th>
-                        <th>Year</th>
+                            <th>Nombre</th>
+                            <th>Cantidad</th>
+                            <th>Precio</th>
                         </tr>
                     </thead>
-                        <tbody>
-                            @foreach (Cart::content() as $item )
+                    <tbody>
+                        @foreach (Cart::content() as $item)
                             <tr>
-                                <td>{{$item->nombre}}</td>
-                                <td>{{$item->descripcion}}</td>
-                                <td>{{number_format($item->qty * $item->precio,2)}}</td>
+                                <td>{{ $item->name }}</td>
+                                <td>{{ $item->qty}} </td>
+                                <td>{{ number_format($item->qty * $item->price, 2) }}</td>
                             </tr>
-                            @endforeach
-                            <tr><td>Total{{Cart::total()}}</td></tr>
-                            
-                        </tbody>
-            </table>
-        </div>
+                        @endforeach
+                        <tr>
+                            <td>Total{{ Cart::total() }}</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
             <p><a href="/vercarrito">Mostrar Carrito</a></p>
         @endif
     </div>
